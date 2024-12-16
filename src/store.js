80566cd2aa978e5d4ec0e43ae8b62eac7b8638c5
Mertcan Kurt <mertcankurt25@gmail.com>
2024-12-16 16:16:28 +0300

    installed thunk - logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,applyMiddleware } from '@reduxjs/toolkit';
 import authReducer from './reducers/authReducer';
+import {thunk} from 'redux-thunk';
+import logger from 'redux-logger';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     logout: authReducer,
   },
-});
+},applyMiddleware(thunk,logger));
 
 export default store;
