e9b3c418f578a5ca9a011517121fb0e6aa7dd839
Mertcan Kurt <mertcankurt25@gmail.com>
2024-12-19 16:31:49 +0300

    installs shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,79 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-    
-      'text-light':'#FFFFFF',
-      'text-dark':'#252B42',
-      'bg-dark':'#252B42',
-      'text-price':'#23856D',
-      'primary-blue':'#23A6F0',
-      'background-light':'#FAFAFA',
-      'second-text-color':'#737373',
-      'newRed':'#E74040',
-      'disable-element-color':'#8EC2F2',
-      'light-green':'#23856D',
-      'star-yellow':'#F3CD03',
-    },
-    extend: {},
+  	colors: {
+  		'text-light': '#FFFFFF',
+  		'text-dark': '#252B42',
+  		'bg-dark': '#252B42',
+  		'text-price': '#23856D',
+  		'primary-blue': '#23A6F0',
+  		'background-light': '#FAFAFA',
+  		'second-text-color': '#737373',
+  		newRed: '#E74040',
+  		'disable-element-color': '#8EC2F2',
+  		'light-green': '#23856D',
+  		'star-yellow': '#F3CD03'
+  	},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
   plugins: [
     function ({ addVariant }) {
         addVariant('child', '& > *');
         addVariant('child-hover', '& > *:hover');
-    }
+    },
+      require("tailwindcss-animate")
 ],
 }
